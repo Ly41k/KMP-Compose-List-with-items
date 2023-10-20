3e89d1176fcb4983674306748c5cb9cf1592e072
YuriyG <mym21072011@gmail.com>
2023-10-20 18:20:35 +0300

    Init project

---
+++ settings.gradle.kts
@@ -0,0 +1,19 @@
+enableFeaturePreview("TYPESAFE_PROJECT_ACCESSORS")
+pluginManagement {
+    repositories {
+        google()
+        gradlePluginPortal()
+        mavenCentral()
+    }
+}
+
+dependencyResolutionManagement {
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "KMP_Compose_List_with_items"
+include(":androidApp")
+include(":shared")
